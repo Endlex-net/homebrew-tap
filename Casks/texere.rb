e499dce79d98f9b193bdc3029805ca4e0b2c6b6d
Endlex <macr@dustess.com>
2026-08-14 18:48:10 +0800

    chore: update texere to 0.2.5-alpha

--- Casks/texere.rb
+++ Casks/texere.rb
@@ -1,8 +1,8 @@
 cask "texere" do
-  version "0.2.4-alpha"
-  sha256 "f3e4f3059c1c4af61393b513b632f20bd812ead97dcc69f62d7c830ef3df24bc"
+  version "0.2.5-alpha"
+  sha256 "e34365a2eb7a67c0b38d8875f934b5935e05518a0176496f83bd893a9c5d44d2"
 
-  url "https://github.com/Endlex-net/Texere/releases/download/v0.2.4-alpha/Texere-0.2.4-alpha-aarch64.dmg"
+  url "https://github.com/Endlex-net/Texere/releases/download/v0.2.5-alpha/Texere-0.2.5-alpha-aarch64.dmg"
   name "Texere"
   desc "Quick draft tool for deep input workflows"
   homepage "https://github.com/Endlex-net/Texere"
